3ba9f34d302a5efcb7337944ba96b1f79b2704ae
Rhithor <reynesh2005@gmail.com>
2026-02-06 16:32:29 +0530

    Added websockets

--- backend/src/controllers/disasterController.js
+++ backend/src/controllers/disasterController.js
@@ -1,4 +1,5 @@
 import { prisma } from "../config/db.js";
+import { io } from "../server.js";
 
 export const getDisasterWatchlist = async (req, res) => {
     try {
@@ -24,15 +25,21 @@ export const getDisasterWatchlist = async (req, res) => {
 
 // Victim chooses to join a specific disaster watchlist(essentially adding them to the disaster table)
 export const joinDisaster = async (req, res) => {
-
     const { disasterId } = req.body;
     try {
         console.log("User ID from token:", req.user.id);
         console.log("Disaster ID from body:", disasterId);
         const updatedVictim = await prisma.victim.update({
             where: { id: req.user.id},
-            data: { disasterId: disasterId }
+            data: { disasterId: disasterId },
+            include: {disaster: true}
         });
+        io.emit("NEW_EMERGENCY", {
+            victimName: updatedVictim.name,
+            disasterName: updatedVictim.disaster.name,
+            emergencyType: updatedVictim.emergency_type
+        });
+        console.log(`Broadcast sent: ${updatedVictim.name} joined ${updatedVictim.disaster.name}`);
         res.status(200).json({ message: "You have been added to the disaster watchlist"})
     } catch (error) {
         console.error("DETAILED ERROR:", error);
--- backend/src/server.js
+++ backend/src/server.js
@@ -7,12 +7,24 @@ import authRoutes from "./routes/authRoutes.js";
 import disasterRoutes from "./routes/disasterRoutes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { Server } from "socket.io";
+import http from "http"
+
+
 
 config();
 connectDB();
 
 
 const app = express();
+const server = http.createServer(app);
+export const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
 app.use(express.json());
 app.use(cors({
     origin: "http://localhost:3000",
@@ -21,6 +33,13 @@ app.use(cors({
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
+io.on('connection', (socket) => {
+    console.log(`Live connection established: ${socket.id}`);
+    socket.on('disconnect', () => {
+      console.log(`User disconnected: ${socket.id}`); 
+    })
+});
 //API routes
 app.use("/api/victims", victimRoutes);
 app.use("/api/volunteers", volunteerRoutes);
@@ -28,11 +47,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/disasters", disasterRoutes);
 
 const PORT = 5000;
-const server = app.listen(PORT, () => {
-    console.log(`ResQ Server running on PORT ${PORT}`);
-
-});
-
+server.listen(PORT, () => {
+    console.log(`ResQ Server(Live) running on PORT ${PORT} `);
+})
 
 // Handle unhandled promise rejections
 process.on("unhandledRejection", (err) => {
